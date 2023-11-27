7c4188ae34d3a95cad63194d2e0eaa8b86284a5a
StainnL <tristan.lee@uleth.ca>
2023-11-27 11:23:39 -0700

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$("#box1").on("click", function () {
+    $("#jhinImage").attr('src', 'images/JHINHAPPY.png');
+});
+$("#box2").on("click", function () {
+    $("#jhinImage").attr('src', 'images/JHINSAD.png');
+});
+$("#box3").on("click", function () {
+    $("#jhinImage").attr('src', 'images/JHINANGRY.png');
+});
+$("#box4").on("click", function () {
+    $("#jhinImage").attr('src', 'images/JHINSTRESSED.png');
+});
+$("#box5").on("click", function () {
+    $("#jhinImage").attr('src', 'images/JHINSCARED.png');
+});
